3ecb99741aee9cb64e66bcf18722b63e6e251ab6
Jean-Philippe GUIOT <jpg@Mac.lan>
2026-01-08 23:16:25 +0100

    Rename class name from CreateMxRollups to CreateMXRollups in migration file

--- db/migrate/20241121000001_create_mx_rollups.rb
+++ db/migrate/20241121000001_create_mx_rollups.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateMxRollups < ActiveRecord::Migration[7.0]
+class CreateMXRollups < ActiveRecord::Migration[7.0]
   def change
     create_table :mx_rollups do |t|
       t.string :mx_hostname, null: false
